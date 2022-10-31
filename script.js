ed4565a5848aa1c2c55c835c61dfd986930cc400
Hossein <djalicrt@gmail.com>
2022-10-31 11:20:14 +0330

    Words added. word shuffle func added.

---
+++ script.js
@@ -0,0 +1,13 @@
+const initGame = () => {
+    let randomObj = words[Math.floor(Math.random() * words.length)]
+    let wordArrays = randomObj.word.split("")
+    for (let i = wordArrays.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1))
+        [wordArrays[i] ,wordArrays[j] = wordArrays[j] , wordArrays[i]]
+    }
+    
+    
+    console.log(wordArrays)
+}
+
+initGame()
---
+++ word.js
@@ -0,0 +1,18 @@
+const words = [
+    {
+        word: "Book",
+        hint: "An Object for reading"
+    },
+    {
+        word: "Pencil",
+        hint: "An Object for writing"
+    },
+    {
+        word: "BlackBoard",
+        hint: "An Object in Class"
+    },
+    {
+        word: "Smart",
+        hint: "Some People have it"
+    }
+]
