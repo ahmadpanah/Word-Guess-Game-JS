9c33be21a8875442cf09c60f7a58a9f5d259aee3
Hossein <h.ahmadpanah@asiatech.cloud>
2022-11-06 01:16:22 +0330

    Fix Bug

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const initGame = () => {
     let randomObj = words[Math.floor(Math.random() * words.length)]
     let wordArrays = randomObj.word.split("")
     for (let i = wordArrays.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1))
-        [wordArrays[i] ,wordArrays[j] = wordArrays[j] , wordArrays[i]]
+        let j = Math.floor(Math.random() * (i + 1));
+        [wordArrays[i], wordArrays[j]] = [wordArrays[j], wordArrays[i]];
     }
     
     
