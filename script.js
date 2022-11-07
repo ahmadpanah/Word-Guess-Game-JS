fd4f7447152d81f7937f1389d5c4a2ab2d0ad3e4
Hossein <djalicrt@gmail.com>
2022-11-07 10:02:37 +0330

    Solve Function is done 🙂

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 
 
 const wordText = document.querySelector(".word")
+const hintText = document.querySelector(".hint span")
+const refreshBtn = document.querySelector(".refresh-word") 
+const checkBtn = document.querySelector(".check-word")
+const inputBox = document.querySelector("input")  
+let correctWord;
 
 const initGame = () => {
     let randomObj = words[Math.floor(Math.random() * words.length)]
@@ -11,7 +16,21 @@ const initGame = () => {
     }
     
     wordText.innerHTML = wordArrays.join("");
-    console.log(wordArrays)
+    hintText.innerHTML = randomObj.hint;
+    
+    correctWord = randomObj.word.toLowerCase()
+    
+    console.log(randomObj)
 }
 
 initGame()
+
+const solveWord = () => {
+    let userWord = inputBox.value.toLocaleLowerCase()
+    if(userWord !== correctWord) return alert (`Vay! ${userWord} is not Correct! 👋`)
+    alert(`Doroste! ${userWord} is Correct! ✔️`)
+    console.log(userWord)
+}
+
+refreshBtn.addEventListener("click" , initGame)
+checkBtn.addEventListener("click" , solveWord)
