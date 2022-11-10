a229894d86ab32cde2dec4b591a73390e0a9706a
Aisa <67589592+Aisa-Asghari@users.noreply.github.com>
2022-11-10 17:28:53 +0330

    New Words Added ➕

--- word.js
+++ word.js
@@ -1,18 +1,106 @@
-const words = [
-    {
-        word: "Book",
-        hint: "An Object for reading"
-    },
-    {
-        word: "Pencil",
-        hint: "An Object for writing"
-    },
-    {
-        word: "BlackBoard",
-        hint: "An Object in Class"
-    },
-    {
-        word: "Smart",
-        hint: "Some People have it"
-    }
-]
+const noviceWords = [
+  {
+    word: "Arrival",
+    hint: "Coming to a place",
+  },
+  {
+    word: "Everywhere",
+    hint: "In all places",
+  },
+  {
+    word: "Rainy",
+    hint: "With a lot of rain",
+  },
+  {
+    word: "Better",
+    hint: "More good",
+  },
+  {
+    word: "Bookshop",
+    hint: "A shop that sells books",
+  },
+  {
+    word: "Cheerful",
+    hint: "Happy",
+  },
+  {
+    word: "Crowd",
+    hint: "A lot of people together",
+  },
+  {
+    word: "Discuss",
+    hint: "Talk about something",
+  },
+  {
+    word: "Face",
+    hint: "The front part of your head",
+  },
+  {
+    word: "Finish",
+    hint: "Stop happening",
+  },
+  {
+    word: "Follow",
+    hint: "Come or go after somebody or something",
+  },
+  {
+    word: "Gram",
+    hint: "A measure of weight",
+  },
+  {
+    word: "Into",
+    hint: "To the middle or inside of something",
+  },
+  {
+    word: "Left",
+    hint: "Form of leave",
+  },
+  {
+    word: "Line",
+    hint: "A long thing mark",
+  },
+  {
+    word: "Local",
+    hint: "Of a place near you",
+  },
+  {
+    word: "Map",
+    hint: "A drawing of a town",
+  },
+  {
+    word: "Metal",
+    hint: "Iron",
+  },
+  {
+    word: "Missing",
+    hint: "Lost",
+  },
+  {
+    word: "Morning",
+    hint: "The first part of the day",
+  },
+];
+
+const standardWords = [
+  { word: "Field", hint: "A piece of land" },
+  { word: "Nonstop", hint: "Not stopping" },
+  { word: "Fascinating", hint: "Very interesting" },
+  { word: "Tank", hint: "A container for liquids" },
+  { word: "Aim", hint: "Point something" },
+  { word: "Waste", hint: "Unwanted materials" },
+  { word: "Harm", hint: "To damage something" },
+  { word: "Sea", hint: "The large area of salty water" },
+  { word: "Packaging", hint: "The process of wrapping" },
+  { word: "Disease", hint: "An illness" },
+];
+
+const expertWords = [
+  { word: "Mentor", hint: "An experienced person" },
+  { word: "Tatty", hint: "Shabby" },
+  { word: "Resilience", hint: "Toughness" },
+  { word: "Medicority", hint: "Second rate" },
+  { word: "Notorious", hint: "Infamous" },
+  { word: "Emerge", hint: "To appear" },
+  { word: "Beneath", hint: "Underneath" },
+  { word: "Slink", hint: "Creep" },
+];
