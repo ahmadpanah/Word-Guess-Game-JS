1437843b5ad4d89dbe186dfae19216729a323383
Aisa <67589592+Aisa-Asghari@users.noreply.github.com>
2022-11-10 17:53:18 +0330

    Starting Point, Leveling System, Score Counter
    + Some changes which where made in order not to show the random word's characters in their correct order

--- script.js
+++ script.js
@@ -1,55 +1,99 @@
-const wordText = document.querySelector(".word")
-const hintText = document.querySelector(".hint span")
-const timeText = document.querySelector(".time b")
-const refreshBtn = document.querySelector(".refresh-word") 
-const checkBtn = document.querySelector(".check-word")
-const inputBox = document.querySelector("input")  
-let correctWord , timer;
+const wordText = document.querySelector(".word");
+const hintText = document.querySelector(".hint span");
+const timeText = document.querySelector(".time b");
+const startOverBtn = document.querySelector(".start-over");
+const checkBtn = document.querySelector(".check-word");
+const inputBox = document.querySelector("input");
+let correctWord, timer;
 
-const initTimer = maxTime => {
+var noviceBtn = document.getElementById("novice");
+var standardBtn = document.getElementById("standard");
+var expertBtn = document.getElementById("expert");
 
-    timer = setInterval(() => {
-        if (maxTime > 0) {
-            maxTime--
-            return timeText.innerHTML = maxTime
-        }
-        // clearInterval(timer)
-        alert("Time Over! 😎")
-        initGame()
-    },1000)
+var startPnl = document.getElementById("start");
+var mainPnl = document.getElementById("main");
 
+var scoreCounter = 0;
+const scoreText = document.querySelector(".score span");
+const allText = document.querySelector(".score b");
+scoreText.innerHTML = scoreCounter;
 
-}
+var levelInfo;
 
-const initGame = () => {
-    clearInterval(timer)
-    initTimer(20)
-    let randomObj = words[Math.floor(Math.random() * words.length)]
-    let wordArrays = randomObj.word.split("")
-    for (let i = wordArrays.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        [wordArrays[i], wordArrays[j]] = [wordArrays[j], wordArrays[i]];
+const initTimer = (maxTime) => {
+  timer = setInterval(() => {
+    if (maxTime > 0) {
+      maxTime--;
+      return (timeText.innerHTML = maxTime);
     }
-    
-    wordText.innerHTML = wordArrays.join("");
-    hintText.innerHTML = randomObj.hint;
-    
-    correctWord = randomObj.word.toLowerCase()
-    
-    console.log(randomObj)
-}
+    // clearInterval(timer)
+    alert("Time is up! 😎");
+    scoreCounter = 0;
+    scoreText.innerHTML = scoreCounter;
+    initGame(levelInfo.Type, levelInfo.Time);
+  }, 1000);
+};
 
-initGame()
+const initGame = (words, time) => {
+  allText.innerHTML = words.length;
+  clearInterval(timer);
+  initTimer(time);
+  let randomObj = words[Math.floor(Math.random() * words.length)];
+  console.log(randomObj);
+  let wordArrays = randomObj.word.split("");
+  for (let i = wordArrays.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [wordArrays[i], wordArrays[j]] = [wordArrays[j], wordArrays[i]];
+  }
 
-const solveWord = () => {
-    let userWord = inputBox.value.toLocaleLowerCase()
-    if(!userWord)  return alert ("Please Enter a Word")
-    if(userWord !== correctWord) return alert (`Vay! ${userWord} is not Correct! 👋`)
-   inputBox.value = "";
-    alert(`Doroste! ${userWord} is Correct! ✔️`)
-    initGame();
-    console.log(userWord)
-}
+  if (wordArrays.join("").toLowerCase() == randomObj.word.toLowerCase()) return;
 
-refreshBtn.addEventListener("click" , initGame)
-checkBtn.addEventListener("click" , solveWord)
+  wordText.innerHTML = wordArrays.join("");
+  hintText.innerHTML = randomObj.hint;
+
+  correctWord = randomObj.word.toLowerCase();
+};
+
+noviceBtn.onclick = () => {
+  levelInfo = { Type: noviceWords, Time: 21 };
+  initGame(levelInfo.Type, levelInfo.Time);
+  pnlVisibility();
+};
+
+standardBtn.onclick = () => {
+  levelInfo = { Type: standardWords, Time: 16 };
+  initGame(levelInfo.Type, levelInfo.Time);
+  pnlVisibility();
+};
+
+expertBtn.onclick = () => {
+  levelInfo = { Type: expertWords, Time: 11 };
+  initGame(levelInfo.Type, levelInfo.Time);
+  pnlVisibility();
+};
+
+const pnlVisibility = () => {
+  startPnl.setAttribute("class", "d-none");
+  mainPnl.classList.remove("d-none");
+};
+
+checkBtn.onclick = () => {
+  let userWord = inputBox.value.toLocaleLowerCase();
+  if (!userWord) return alert("Please Enter a Word");
+  if (userWord !== correctWord)
+    return alert(`Vay! ${userWord} is not Correct! 👋`);
+  inputBox.value = "";
+  scoreCounter++;
+  scoreText.innerHTML = scoreCounter;
+  if (scoreCounter == levelInfo.Type.length) {
+    alert(`Winner Shodi! 🎉`);
+    startOverBtn.onclick();
+  } else {
+    alert(`Doroste! ${userWord} is Correct! ✔️`);
+    initGame(levelInfo.Type, levelInfo.Time);
+  }
+};
+
+startOverBtn.onclick = () => {
+  document.location.reload();
+};
